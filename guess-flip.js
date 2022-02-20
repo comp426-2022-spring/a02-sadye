ef80e600997e1bc53624bef7051189ea8c06d3d7
sadye <sadye@live.unc.edu>
2022-02-20 16:54:36 -0500

    guess-flip

---
+++ guess-flip.js
@@ -0,0 +1,15 @@
+import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.js";
+// Call the coinFlip function and put the return into STDOUT
+const args = require('minimist')(process.argv.slice(2)) 
+
+args['call']
+
+const call = args.call
+if(call === 'heads' | call === 'tails'){
+    console.log(flipACoin(call))
+} else {
+    throw
+}
+totalFlips = coinFlips(flips)
+console.log(totalFlips)
+console.log(countFlips(totalFlips))
