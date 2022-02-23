b33d1c4f108dc046472acc09035818d8caf99560
sadye <sadye@live.unc.edu>
2022-02-23 13:53:21 -0500

    finally got it at a good point

--- flips.js
+++ flips.js
@@ -1,11 +1,13 @@
 // Import the coinFlip function from your coin.mjs file
 import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.js";
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 // Call the coinFlip function and put the return into STDOUT
 const args = require('minimist')(process.argv.slice(2)) 
 
-args['flips']
+args['number']
 
-const flips = args.flips || 1
-totalFlips = coinFlips(flips)
+const flips = args.number || 1
+const totalFlips = coinFlips(flips)
 console.log(totalFlips)
 console.log(countFlips(totalFlips))
--- guess-flip.js
+++ guess-flip.js
@@ -1,6 +1,9 @@
 import { coinFlip, coinFlips, countFlips, flipACoin } from "./modules/coin.js";
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 // Call the coinFlip function and put the return into STDOUT
 const args = require('minimist')(process.argv.slice(2)) 
+
 
 args['call']
 
@@ -8,8 +11,6 @@ const call = args.call
 if(call === 'heads' | call === 'tails'){
     console.log(flipACoin(call))
 } else {
-    console.error();
+    console.error("Error: no input.");
+    console.log("Usage: node guess-flip --call=[heads|tails]")
 }
-totalFlips = coinFlips(flips)
-console.log(totalFlips)
-console.log(countFlips(totalFlips))
